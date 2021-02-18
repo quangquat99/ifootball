4d4c1f0f144f35e8b3f5469a3614ae4d01a76057
quangquat99 <quangphuet99@gmail.com>
2021-02-18 21:56:46 +0700

    create code

---
+++ src/main/java/quang/ph/ifootball/IFootballApplication.java
@@ -0,0 +1,13 @@
+package quang.ph.ifootball;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IFootballApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IFootballApplication.class, args);
+	}
+
+}
---
+++ src/test/java/quang/ph/ifootball/IFootballApplicationTests.java
@@ -0,0 +1,13 @@
+package quang.ph.ifootball;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IFootballApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
